3e1f8e8060ef1fc6f404b2f329580df5bac2e26a
josueaosilverio <josueaosilverio@gmail.com>
2019-02-23 20:49:09 +0000

    shortened if

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -14,9 +14,7 @@ void loop() {
   digitalWrite(LED_BUILTIN, LOW);
   delay(timebtw);
    
-    if(timebtw > 0 && timebtw%2 == 0){
-      timebtw -= 50;
-    }else if (timebtw <= 0 && timebtw%2 == 0){
+    if (timebtw <= 0 && timebtw%2 == 0){
       timebtw += 51;
     }else if (timebtw < 1001 && timebtw%2 == 1){
       timebtw += 50;
