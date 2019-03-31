190a8e179a69ade2d684a9d3d8b3be52f958375d
josueaosilverio <josueaosilverio@gmail.com>
2019-03-31 18:57:31 +0100

    brinquedo stuff

--- ServoBrinquedo/brinquedoLightStuff.ino
+++ ServoBrinquedo/brinquedoLightStuff.ino
@@ -1,12 +1,24 @@
 #include <Servo.h>
 
 Servo servo;
+int rEye;
+int lEye;
+int servoPos = 90;
 
 void setup(){
     servo.attach(9);
-    servo.write(90);
+    servo.write(servoPos);
 }
 
 void loop(){
+    rEye = analogRead(0);
+    lEye = analogRead(1);
+
+    rEye > lEye ? servoPos++; 
+    rEye < lEye ? servoPos--;
+    servoPos > 180 ? servoPos = 180; 
+    servoPos < 0 ? servoPos = 0;
+
+    servo.write(servoPos); 
 
 }
