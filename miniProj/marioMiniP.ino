268fe03451cab921f9fd64d7caad52261b035ee1
josueaosilverio <josueaosilverio@gmail.com>
2019-04-24 18:12:23 +0100

    fixes on the data gather

--- miniProj/marioMiniP.ino
+++ miniProj/marioMiniP.ino
@@ -101,7 +101,7 @@
 #define NOTE_D8  4699
 #define NOTE_DS8 4978
 
-#define melodyPin 5
+#define melodyPin 6
 //Mario main theme melody
 int melody[] = {
   NOTE_E7, NOTE_E7, 0, NOTE_E7,
--- miniProj/miniproj.ino
+++ miniProj/miniproj.ino
@@ -4,8 +4,8 @@ int sharpWammy = A1;
 int BuzzerL = 5;
 int BuzzerR = 6;
 
-int pitch;
-int wammy;
+int pitch = 0;
+int wammy = 0;
 
 //TODO CALIBRATION
 int pitchMin;
@@ -26,12 +26,18 @@ void setup()
 void loop()
 {
     //TODO Add Mapping
-    pitch = analogRead(sharpPitch);
+    //pitch = analogRead10(sharpPitch);
+    pitch = analogRead8020(pitch, analogRead(sharpPitch));
 
     //TODO Add Mapping
-    wammy = analogRead(sharpWammy);
+    //wammy = analogRead8020(sharpWammy);
 
-    playTone(pitch, wammy);
+    //playTone(pitch, wammy);
+
+    Serial.println(pitch);
+    tone(BuzzerR, pitch);
+
+    //Serial.println(wammy);
 
     /*if((pitch >= pitchMin && pitch <= pitchMax) && (wammy >= wammyMin && wammy <= wammyMax)){
         playTone(pitch, wammy);
@@ -46,8 +52,12 @@ void loop()
 
 void playTone(int tonePitch, int toneWammy)
 {
-    tone(BuzzerL, tonePitch, toneWammy);
-    tone(BuzzerR, tonePitch, toneWammy);
+    tone(BuzzerL, tonePitch);
+    delay(toneWammy);
+    noTone(BuzzerL);
+    tone(BuzzerR, tonePitch);
+    delay(toneWammy);
+    noTone(BuzzerR);
 }
 
 void stopTone()
@@ -55,3 +65,30 @@ void stopTone()
     noTone(BuzzerL);
     noTone(BuzzerR);
 }
+
+int analogRead8020(int oldVal, int newVal)
+{
+    return round((0.9 * oldVal) + (0.1 * newVal));
+};
+
+int analogRead10(int pin)
+{
+    int values[10];
+    int avg;
+    for (int i = 0; i < 10; i++)
+    {
+        values[i] = analogRead(pin);
+    }
+    for (int i = 0; i < 10; i++)
+    {
+        avg += values[i];
+    }
+
+    avg = avg/10;
+    if (avg < 0)
+    {
+        avg = avg*-1;
+    }
+
+    return avg;
+};
